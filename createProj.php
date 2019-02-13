982a9021585bccfde59ce7f9f2314db830134c98
Ashish Sanu <sanu.labh@gmail.com>
2019-02-13 12:08:50 +0530

    First Commit

---
+++ createProj.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'php_action/db_connect.php';
+
+echo 'Post hua kuch';
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$projectID=$_POST['projectID'];
+$projectName=$_POST['projectName'];
+$projectTeam=$_POST['projectTeam'];
+$projectDuration=$_POST['projectDuration'];
+$projectSD=$_POST['projectSD'];
+$projectED=$_POST['projectED'];
+
+echo 'Data You Entered are...<br>';
+echo 'Project ID :'. $projectID.' <br>';
+echo 'Project Name :'. $projectName .'<br>';
+echo 'Project Team :'. $projectTeam.' <br>';
+echo 'Project Start Date :'. $projectSD .'<br>';
+echo 'Project End Date :'. $projectED .'<br>';
+
+$sql = "INSERT INTO project (project_id, project_name, project_team, project_duration, project_start_date, project_end_date) 
+				VALUES ('$projectID', '$projectName', '$projectTeam', '$projectDuration', '$projectSD', '$projectED')";
+
+if($connect->query($sql) === TRUE){
+    echo 'Database dekho Project add ho gaya';
+}
+ else {
+    echo 'Kuch Galti kar diye';
+}
---
+++ createProject.php
@@ -0,0 +1,53 @@
+<?php require_once 'php_action/db_connect.php';
+ require_once 'includes/header.php'; 
+ 
+ if($_POST){
+     echo 'Post hua kuch';
+     header('location: http://localhost:8085/inventory/dashboard.php');	
+ }
+ 
+ ?>
+
+<!-- Create Project -->
+<form method="post" action="createProj.php">
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Project ID</label>
+      <input type="text" class="form-control" id="inputEmail4" name="projectID" placeholder="Project ID should be Unique">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Project Name</label>
+      <input type="text" class="form-control" id="inputPassword4" name="projectName" placeholder="Project Name">
+    </div>
+  </div>
+    
+    <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Project Team</label>
+      <input type="text" class="form-control" id="inputEmail4" name="projectTeam" placeholder="Name of the Team">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Project Duration</label>
+      <input type="text" class="form-control" id="inputPassword4" name="projectDuration" placeholder="Project Duration">
+    </div>
+  </div>
+    
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Project Start Date</label>
+      <input type="date" class="form-control" id="inputEmail4" name="projectSD" placeholder="Name of the Team">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Project End Date(Can be left blank)</label>
+      <input type="date" class="form-control" id="inputPassword4" name="projectED" placeholder="Project Duration">
+    </div>
+  </div>
+ 
+  
+  
+  <button type="submit" class="btn btn-primary">Create Project</button>
+</form>
+
+
+
+<?php require_once 'includes/footer.php'; ?>
