cbf7ac7b8eea8c3d883503a6031c5655d26c0813
Ashish Sanu <sanu.labh@gmail.com>
2019-02-15 14:25:33 +0530

    Pushing new DB schema

--- database/store.sql
+++ database/store.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 04, 2018 at 12:42 PM
--- Server version: 5.7.21-0ubuntu0.16.04.1
--- PHP Version: 7.0.28-0ubuntu0.16.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Feb 15, 2019 at 09:54 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -33,6 +35,13 @@ CREATE TABLE `brands` (
   `brand_status` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_name`, `brand_active`, `brand_status`) VALUES
+(1, 'Axe', 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +54,13 @@ CREATE TABLE `categories` (
   `categories_active` int(11) NOT NULL DEFAULT '0',
   `categories_status` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`categories_id`, `categories_name`, `categories_active`, `categories_status`) VALUES
+(1, 'Deodrant', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -106,6 +122,89 @@ CREATE TABLE `product` (
   `status` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `product_image`, `brand_id`, `categories_id`, `quantity`, `rate`, `active`, `status`) VALUES
+(1, 'abc', '../assests/images/stock/5762913615c63b721f0450.png', 1, 1, '12', '125', 2, 2),
+(2, 'ICT', '../assests/images/stock/6521557655c63cbd75868c.png', 1, 1, '123', '8970', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `project_id` int(11) NOT NULL,
+  `project_name` varchar(500) NOT NULL,
+  `project_team` varchar(300) NOT NULL,
+  `project_duration` varchar(300) NOT NULL,
+  `project_start_date` date NOT NULL,
+  `project_end_date` date DEFAULT NULL,
+  `status` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`project_id`, `project_name`, `project_team`, `project_duration`, `project_start_date`, `project_end_date`, `status`) VALUES
+(1, 'abcdef', '84NRV-6CJR6-DBDXH-FYTBF-4X49V', 'windows 8', '2019-02-15', '0000-00-00', 2),
+(2, 'Abhishek kr', '1', '10 Months', '2019-02-15', '2019-02-20', 2),
+(3, '', '', '', '0000-00-00', '0000-00-00', 2),
+(4, '', '', '', '0000-00-00', '0000-00-00', 1),
+(5, '', '', '', '0000-00-00', '0000-00-00', 1),
+(6, '', '', '', '0000-00-00', '0000-00-00', 1),
+(7, '1234', '12233', '211', '2019-01-12', '0000-00-00', 1),
+(8, '', '', '', '0000-00-00', '0000-00-00', 1),
+(9, '', '', '', '0000-00-00', '0000-00-00', 1),
+(10, '', '', '', '0000-00-00', '0000-00-00', 1),
+(11, '', '', '', '0000-00-00', '0000-00-00', 1),
+(12, '', '', '', '0000-00-00', '0000-00-00', 1),
+(13, '', '', '', '0000-00-00', '0000-00-00', 1),
+(14, '', '', '', '0000-00-00', '0000-00-00', 1),
+(15, '', '', '', '0000-00-00', '0000-00-00', 1),
+(16, '', '', '', '0000-00-00', '0000-00-00', 1),
+(17, '', '', '', '0000-00-00', '0000-00-00', 1),
+(18, '', '', '', '0000-00-00', '0000-00-00', 1),
+(19, '', '', '', '0000-00-00', '0000-00-00', 1),
+(20, '', '', '', '0000-00-00', '0000-00-00', 1),
+(21, '', '', '', '0000-00-00', '0000-00-00', 1),
+(22, '', '', '', '0000-00-00', '0000-00-00', 1),
+(23, 'rgwergw', 'rgwer', '2', '0000-00-00', '0000-00-00', 1),
+(1282, 'fgbfgbfg', 'aasas', '2', '2019-02-15', '2019-03-01', 1),
+(1283, 'abhishek', '4 months', '1', '2019-02-14', '2019-02-15', 1),
+(1284, 'Ashish', 'ashish', '18', '2019-02-01', '2019-02-01', 1),
+(1285, '', '', '', '0000-00-00', '0000-00-00', 1),
+(1286, 'Ashish Sanu', 'Delta', '12 Months', '2019-02-13', '2019-02-27', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system`
+--
+
+CREATE TABLE `system` (
+  `system_id` varchar(300) NOT NULL,
+  `serial_number` varchar(300) NOT NULL,
+  `product_key` varchar(300) NOT NULL,
+  `oem` varchar(300) NOT NULL,
+  `date_of_allocation` date NOT NULL,
+  `system_user` varchar(300) NOT NULL,
+  `project` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `system`
+--
+
+INSERT INTO `system` (`system_id`, `serial_number`, `product_key`, `oem`, `date_of_allocation`, `system_user`, `project`) VALUES
+('123', 'wedweadaf', 'gasdfgvsdfgv', 'gsfdgdsfgsdf', '2019-02-15', 'gfsdgdsf', 'sdgfdsg'),
+('2', 'ahbdwh12334hiufv fu', 'asdfghjk123456sdfghjerty', 'windows', '2019-02-21', 'Ashish', 'Bada Wala'),
+('3', 'vn n', 'mghm', 'jgmg', '2019-02-26', 'vkj', 'bmbhjm');
+
 -- --------------------------------------------------------
 
 --
@@ -161,6 +260,18 @@ ALTER TABLE `product`
   ADD PRIMARY KEY (`product_id`);
 
 --
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`project_id`);
+
+--
+-- Indexes for table `system`
+--
+ALTER TABLE `system`
+  ADD PRIMARY KEY (`system_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -174,12 +285,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `categories_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `categories_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `orders`
 --
@@ -194,12 +305,18 @@ ALTER TABLE `order_item`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1287;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
